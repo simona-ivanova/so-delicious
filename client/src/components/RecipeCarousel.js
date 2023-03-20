7067f02f11bfcfba02ef474daca10d7811c44a3c
Simona Ivanova <116807754+simona-ivanova@users.noreply.github.com>
2023-03-20 16:32:14 +0200

    Add RecipeCarousel component

---
+++ client/src/components/RecipeCarousel.js
@@ -0,0 +1,136 @@
+export const RecipeCarousel = () => {
+    return (
+        <section className="u-align-center u-clearfix u-white u-section-1" id="sec-15c6">
+            <div className="u-clearfix u-sheet u-valign-middle u-sheet-1">
+                <div
+                    className="u-expanded-width u-gallery u-layout-horizontal u-lightbox u-no-transition u-show-text-on-hover u-width-fixed u-gallery-1">
+                    <div className="u-gallery-inner">
+                        <div className="u-effect-fade u-gallery-item u-gallery-item-1"
+                            data-href="https://nicepage.com/c/grid-website-templates">
+                            <div className="u-back-slide" data-image-width="642" data-image-height="969">
+                                <img className="u-back-image u-back-image-1" src="images/38.jpg" />
+                            </div>
+                            <div className="u-over-slide u-shading u-over-slide-1">
+                                <h3 className="u-gallery-heading"></h3>
+                                <p className="u-gallery-text"></p>
+                            </div>
+                        </div>
+                        <div className="u-effect-fade u-gallery-item u-gallery-item-2" data-image-width="500" data-image-height="692"
+                            data-href="https://nicepage.online">
+                            <div className="u-back-slide">
+                                <img className="u-back-image u-back-image-2" src="images/DSC42721.jpg" />
+                            </div>
+                            <div className="u-over-slide u-shading u-over-slide-2">
+                                <h3 className="u-gallery-heading"></h3>
+                                <p className="u-gallery-text"></p>
+                            </div>
+                        </div>
+                        <div className="u-effect-fade u-gallery-item u-gallery-item-3" data-href="https://nicepage.com/joomla-templates">
+                            <div className="u-back-slide" data-image-width="600" data-image-height="777">
+                                <img className="u-back-image u-back-image-3" src="images/DSC0324-21.jpg" />
+                            </div>
+                            <div className="u-over-slide u-shading u-over-slide-3">
+                                <h3 className="u-gallery-heading"></h3>
+                                <p className="u-gallery-text"></p>
+                            </div>
+                        </div>
+                        <div className="u-effect-fade u-gallery-item u-gallery-item-4"
+                            data-href="https://nicepage.com/c/shapes-website-templates">
+                            <div className="u-back-slide" data-image-width="652" data-image-height="772">
+                                <img className="u-back-image u-back-image-4" src="images/------2.jpg" />
+                            </div>
+                            <div className="u-over-slide u-shading u-over-slide-4">
+                                <h3 className="u-gallery-heading"></h3>
+                                <p className="u-gallery-text"></p>
+                            </div>
+                        </div>
+                        <div className="u-effect-fade u-gallery-item u-gallery-item-5" data-image-width="646" data-image-height="938"
+                            data-href="https://nicepage.com/k/youtube-website-templates">
+                            <div className="u-back-slide">
+                                <img className="u-back-image u-back-image-5" src="images/DSC_09344.jpg" />
+                            </div>
+                            <div className="u-over-slide u-shading u-over-slide-5">
+                                <h3 className="u-gallery-heading"></h3>
+                                <p className="u-gallery-text"></p>
+                            </div>
+                        </div>
+                        <div className="u-effect-fade u-gallery-item u-gallery-item-6" data-image-width="768" data-image-height="1152"
+                            data-href="https://nicepage.online">
+                            <div className="u-back-slide">
+                                <img className="u-back-image u-back-image-6" src="images/DSC657311-768x1152.jpg" />
+                            </div>
+                            <div className="u-over-slide u-shading u-over-slide-6">
+                                <h3 className="u-gallery-heading"></h3>
+                                <p className="u-gallery-text"></p>
+                            </div>
+                        </div>
+                        <div className="u-effect-fade u-gallery-item u-gallery-item-7" data-image-width="651" data-image-height="894"
+                            data-href="https://nicepage.best">
+                            <div className="u-back-slide">
+                                <img className="u-back-image u-back-image-7" src="images/DSC3877.jpg" />
+                            </div>
+                            <div className="u-over-slide u-shading u-over-slide-7">
+                                <h3 className="u-gallery-heading"></h3>
+                                <p className="u-gallery-text"></p>
+                            </div>
+                        </div>
+                        <div className="u-effect-fade u-gallery-item u-gallery-item-8" data-image-width="549" data-image-height="824"
+                            data-href="https://nicepage.com/k/clothing-store-website-templates">
+                            <div className="u-back-slide">
+                                <img className="u-back-image u-back-image-8" src="images/--------12.jpg" />
+                            </div>
+                            <div className="u-over-slide u-shading u-over-slide-8">
+                                <h3 className="u-gallery-heading"></h3>
+                                <p className="u-gallery-text"></p>
+                            </div>
+                        </div>
+                        <div className="u-effect-fade u-gallery-item u-gallery-item-9" data-image-width="549" data-image-height="824"
+                            data-href="https://nicepage.cloud">
+                            <div className="u-back-slide" data-image-width="642" data-image-height="816">
+                                <img className="u-back-image u-back-image-9" src="images/DSC_23671.JPG" />
+                            </div>
+                            <div className="u-over-slide u-shading u-over-slide-9">
+                                <h3 className="u-gallery-heading"></h3>
+                                <p className="u-gallery-text"></p>
+                            </div>
+                        </div>
+                    </div>
+                    <a className="u-gallery-nav u-gallery-nav-prev u-grey-70 u-icon-circle u-opacity u-opacity-70 u-spacing-10 u-text-white u-gallery-nav-1"
+                        href="#" role="button">
+                        <span aria-hidden="true">
+                            <svg viewBox="0 0 451.847 451.847">
+                                <path d="M97.141,225.92c0-8.095,3.091-16.192,9.259-22.366L300.689,9.27c12.359-12.359,32.397-12.359,44.751,0
+    c12.354,12.354,12.354,32.388,0,44.748L173.525,225.92l171.903,171.909c12.354,12.354,12.354,32.391,0,44.744
+    c-12.354,12.365-32.386,12.365-44.745,0l-194.29-194.281C100.226,242.115,97.141,234.018,97.141,225.92z"></path>
+                            </svg>
+                        </span>
+                        <span className="sr-only">
+                            <svg viewBox="0 0 451.847 451.847">
+                                <path d="M97.141,225.92c0-8.095,3.091-16.192,9.259-22.366L300.689,9.27c12.359-12.359,32.397-12.359,44.751,0
+    c12.354,12.354,12.354,32.388,0,44.748L173.525,225.92l171.903,171.909c12.354,12.354,12.354,32.391,0,44.744
+    c-12.354,12.365-32.386,12.365-44.745,0l-194.29-194.281C100.226,242.115,97.141,234.018,97.141,225.92z"></path>
+                            </svg>
+                        </span>
+                    </a>
+                    <a className="u-gallery-nav u-gallery-nav-next u-grey-70 u-icon-circle u-opacity u-opacity-70 u-spacing-10 u-text-white u-gallery-nav-2"
+                        href="#" role="button">
+                        <span aria-hidden="true">
+                            <svg viewBox="0 0 451.846 451.847">
+                                <path d="M345.441,248.292L151.154,442.573c-12.359,12.365-32.397,12.365-44.75,0c-12.354-12.354-12.354-32.391,0-44.744
+    L278.318,225.92L106.409,54.017c-12.354-12.359-12.354-32.394,0-44.748c12.354-12.359,32.391-12.359,44.75,0l194.287,194.284
+    c6.177,6.18,9.262,14.271,9.262,22.366C354.708,234.018,351.617,242.115,345.441,248.292z"></path>
+                            </svg>
+                        </span>
+                        <span className="sr-only">
+                            <svg viewBox="0 0 451.846 451.847">
+                                <path d="M345.441,248.292L151.154,442.573c-12.359,12.365-32.397,12.365-44.75,0c-12.354-12.354-12.354-32.391,0-44.744
+    L278.318,225.92L106.409,54.017c-12.354-12.359-12.354-32.394,0-44.748c12.354-12.359,32.391-12.359,44.75,0l194.287,194.284
+    c6.177,6.18,9.262,14.271,9.262,22.366C354.708,234.018,351.617,242.115,345.441,248.292z"></path>
+                            </svg>
+                        </span>
+                    </a>
+                </div>
+            </div>
+        </section>
+    );
+};
