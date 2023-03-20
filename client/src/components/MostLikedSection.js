ef97b3b401c3fb8023ee37b562e0af69d9c58213
Simona Ivanova <116807754+simona-ivanova@users.noreply.github.com>
2023-03-20 16:33:53 +0200

    Add MosLikedSection component

---
+++ client/src/components/MostLikedSection.js
@@ -0,0 +1,95 @@
+export const MostLikedSection = () => {
+    return (
+        <section className="u-align-center u-clearfix u-section-3" id="sec-166e">
+            <div className="u-clearfix u-sheet u-sheet-1">
+                <h2 className="u-text u-text-default-xl u-text-1">
+                    <a href="#"
+                        className="u-active-none u-border-none u-btn u-button-link u-button-style u-hover-none u-none u-text-palette-2-light-1 u-btn-1">Най-харесвани рецепти</a>
+                </h2>
+                <div
+                    className="u-carousel u-gallery u-gallery-slider u-layout-carousel u-lightbox u-no-transition u-show-text-on-hover u-gallery-1"
+                    id="carousel-f5be" data-interval="5000" data-u-ride="carousel">
+                    <ol className="u-absolute-hcenter u-carousel-indicators u-carousel-indicators-1">
+                        <li data-u-target="#carousel-f5be" data-u-slide-to="0" className="u-active u-grey-70 u-shape-circle"
+                            style={{width: 10, height: 10}}></li>
+                        <li data-u-target="#carousel-f5be" data-u-slide-to="1" className="u-grey-70 u-shape-circle"
+                            style={{width: 10, height: 10}}></li>
+                        <li data-u-target="#carousel-f5be" data-u-slide-to="2" className="u-grey-70 u-shape-circle"
+                            style={{width: 10, height: 10}}></li>
+                    </ol>
+                    <div className="u-carousel-inner u-gallery-inner" role="listbox">
+                        <div className="u-active u-carousel-item u-effect-fade u-gallery-item u-carousel-item-1" data-image-width="600"
+                            data-image-height="856" data-href="https://nicepage.com/k/home-improvement-website-templates">
+                            <div className="u-back-slide" data-image-width="550" data-image-height="825">
+                                <img className="u-back-image u-expanded" src="images/DSC88211.jpg" />
+                            </div>
+                            <div className="u-align-center u-over-slide u-shading u-valign-bottom u-over-slide-1">
+                                <h3 className="u-gallery-heading">Sample Title</h3>
+                                <p className="u-gallery-text">Sample Text</p>
+                            </div>
+                            <style data-mode="XL" data-visited="true"></style>
+                            <style data-mode="LG" data-visited="true"></style>
+                            <style data-mode="MD" data-visited="true"></style>
+                            <style data-mode="SM" data-visited="true"></style>
+                            <style data-mode="XS" data-visited="true"></style>
+                        </div>
+                        <div className="u-carousel-item u-effect-fade u-gallery-item u-carousel-item-2">
+                            <div className="u-back-slide" data-image-width="600" data-image-height="856">
+                                <img className="u-back-image u-expanded u-back-image-2" src="images/DSC37431.jpg" />
+                            </div>
+                            <div className="u-align-center u-over-slide u-shading u-valign-bottom u-over-slide-2">
+                                <h3 className="u-gallery-heading">Sample Title</h3>
+                                <p className="u-gallery-text">Sample Text</p>
+                            </div>
+                        </div>
+                        <div className="u-carousel-item u-effect-fade u-gallery-item u-carousel-item-3">
+                            <div className="u-back-slide" data-image-width="500" data-image-height="675">
+                                <img className="u-back-image u-expanded" src="images/DSC862712.jpg" />
+                            </div>
+                            <div className="u-align-center u-over-slide u-shading u-valign-bottom u-over-slide-3">
+                                <h3 className="u-gallery-heading">Sample Title</h3>
+                                <p className="u-gallery-text">Sample Text</p>
+                            </div>
+                        </div>
+                    </div>
+                    <a className="u-absolute-vcenter u-carousel-control u-carousel-control-prev u-grey-70 u-icon-circle u-opacity u-opacity-70 u-spacing-10 u-text-white u-carousel-control-1"
+                        href="#carousel-f5be" role="button" data-u-slide="prev">
+                        <span aria-hidden="true">
+                            <svg viewBox="0 0 451.847 451.847">
+                                <path d="M97.141,225.92c0-8.095,3.091-16.192,9.259-22.366L300.689,9.27c12.359-12.359,32.397-12.359,44.751,0
+    c12.354,12.354,12.354,32.388,0,44.748L173.525,225.92l171.903,171.909c12.354,12.354,12.354,32.391,0,44.744
+    c-12.354,12.365-32.386,12.365-44.745,0l-194.29-194.281C100.226,242.115,97.141,234.018,97.141,225.92z"></path>
+                            </svg>
+                        </span>
+                        <span className="sr-only">
+                            <svg viewBox="0 0 451.847 451.847">
+                                <path d="M97.141,225.92c0-8.095,3.091-16.192,9.259-22.366L300.689,9.27c12.359-12.359,32.397-12.359,44.751,0
+    c12.354,12.354,12.354,32.388,0,44.748L173.525,225.92l171.903,171.909c12.354,12.354,12.354,32.391,0,44.744
+    c-12.354,12.365-32.386,12.365-44.745,0l-194.29-194.281C100.226,242.115,97.141,234.018,97.141,225.92z"></path>
+                            </svg>
+                        </span>
+                    </a>
+                    <a className="u-absolute-vcenter u-carousel-control u-carousel-control-next u-grey-70 u-icon-circle u-opacity u-opacity-70 u-spacing-10 u-text-white u-carousel-control-2"
+                        href="#carousel-f5be" role="button" data-u-slide="next">
+                        <span aria-hidden="true">
+                            <svg viewBox="0 0 451.846 451.847">
+                                <path d="M345.441,248.292L151.154,442.573c-12.359,12.365-32.397,12.365-44.75,0c-12.354-12.354-12.354-32.391,0-44.744
+    L278.318,225.92L106.409,54.017c-12.354-12.359-12.354-32.394,0-44.748c12.354-12.359,32.391-12.359,44.75,0l194.287,194.284
+    c6.177,6.18,9.262,14.271,9.262,22.366C354.708,234.018,351.617,242.115,345.441,248.292z"></path>
+                            </svg>
+                        </span>
+                        <span className="sr-only">
+                            <svg viewBox="0 0 451.846 451.847">
+                                <path d="M345.441,248.292L151.154,442.573c-12.359,12.365-32.397,12.365-44.75,0c-12.354-12.354-12.354-32.391,0-44.744
+    L278.318,225.92L106.409,54.017c-12.354-12.359-12.354-32.394,0-44.748c12.354-12.359,32.391-12.359,44.75,0l194.287,194.284
+    c6.177,6.18,9.262,14.271,9.262,22.366C354.708,234.018,351.617,242.115,345.441,248.292z"></path>
+                            </svg>
+                        </span>
+                    </a>
+                </div>
+            </div>
+        </section>
+
+    );
+};
+
