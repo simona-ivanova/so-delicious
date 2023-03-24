6243acad5db76f4777b0331aed16b2846abea95a
Simona Ivanova <116807754+simona-ivanova@users.noreply.github.com>
2023-03-25 00:24:54 +0200

    Create About component

---
+++ client/src/components/About/About.js
@@ -0,0 +1,55 @@
+import './About.css'
+
+export const About = () => {
+    return (
+        <section className="u-clearfix u-section-1" id="sec-a45c">
+            <div className="u-clearfix u-sheet u-sheet-1">
+                <div className="u-clearfix u-expanded-width u-gutter-10 u-layout-wrap u-layout-wrap-1">
+                    <div className="u-layout">
+                        <div className="u-layout-row">
+                            <div className="u-size-30 u-size-60-md">
+                                <div className="u-layout-col">
+                                    <div className="u-container-style u-image u-layout-cell u-left-cell u-size-60 u-image-1" src="" data-image-width="2048" data-image-height="1365">
+                                        <div className="u-container-layout u-valign-middle u-container-layout-1"></div>
+                                    </div>
+                                </div>
+                            </div>
+                            <div className="u-size-30 u-size-60-md">
+                                <div className="u-layout-col">
+                                    <div className="u-size-30">
+                                        <div className="u-layout-row">
+                                            <div className="u-align-left u-container-style u-layout-cell u-size-30 u-layout-cell-2">
+                                                <div className="u-container-layout u-valign-top u-container-layout-2">
+                                                    <h3 className="u-text u-text-1">We are the experts in quality business strategy</h3>
+                                                </div>
+                                            </div>
+                                            <div className="u-align-left u-container-style u-layout-cell u-right-cell u-size-30 u-layout-cell-3">
+                                                <div className="u-container-layout u-valign-top u-container-layout-3">
+                                                    <p className="u-text u-text-2">Sample text. Click to select the text box. Click again or double click to start editing the text.&nbsp;Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. </p>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <div className="u-size-30">
+                                        <div className="u-layout-row">
+                                            <div className="u-align-left u-container-style u-layout-cell u-size-30 u-layout-cell-4">
+                                                <div className="u-container-layout u-valign-top u-container-layout-4">
+                                                    <p className="u-text u-text-3">Sample text. Click to select the text box. Click again or double click to start editing the text.&nbsp;Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. </p>
+                                                </div>
+                                            </div>
+                                            <div className="u-align-left u-container-style u-custom-color-4 u-layout-cell u-right-cell u-size-30 u-layout-cell-5">
+                                                <div className="u-container-layout u-valign-top u-container-layout-5">
+                                                    <p className="u-text u-text-4">Sample text. Click to select the text box. Click again or double click to start editing the text.&nbsp;Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. </p>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+    );
+};
