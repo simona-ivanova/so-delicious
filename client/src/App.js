423ff293508ff64b0a91d3152fe8d67ac7cf4d25
Simona Ivanova <116807754+simona-ivanova@users.noreply.github.com>
2023-03-25 00:25:31 +0200

    Install react router dom

--- client/src/App.js
+++ client/src/App.js
@@ -1,21 +1,28 @@
+import { Routes, Route } from 'react-router-dom';
+
 import { Header } from "./components/Header/Header";
 import { Footer } from "./components/Footer/Footer";
 import { FooterBottomLine } from "./components/FooterBottomLine/FooterBottomLine";
 import { Home } from "./components/Home/Home";
-
-
+import { Login } from "./components/Login/Login";
+import { Register } from "./components/Register/Register";
+import { About } from './components/About/About';
 
 
 function App() {
   return (
     <>
       <Header />
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/login' element={<Login />} />
+        <Route path='/register' element={<Register />} />
+        <Route path='/about' element={<About />} />
+      </Routes>
+      
 
-      <Home />
 
-    
       <Footer />
-
       <FooterBottomLine />
     </>
   );
