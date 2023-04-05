c532ddc2d95d238f8e5aeffc792b3ea0536328ab
Simona Ivanova <116807754+simona-ivanova@users.noreply.github.com>
2023-04-05 10:53:47 +0300

    Update CatalogItem component

--- client/src/components/Catalog/CatalogItem/CatalogItem.js
+++ client/src/components/Catalog/CatalogItem/CatalogItem.js
@@ -7,28 +7,15 @@ export const CatalogItem = ({
     imageUrl
 }) => {
     return (
-        <div className="u-align-center u-container-style u-list-item u-repeater-item">
-            <div className="u-container-layout u-similar-container u-container-layout-1">
-                <img alt="" className="u-expanded-width u-image u-image-default u-image-1"
-                    data-image-width="649"
-                    data-image-height="920"
-                    src={imageUrl} />
-                <a href="#"
-                    className="u-border-active-palette-2-base u-border-hover-palette-1-base u-border-none u-btn u-button-style u-none u-text-grey-40 u-text-hover-grey-15 u-btn-1"
-                    data-animation-name=""
-                    data-animation-duration="0"
-                    data-animation-delay="0"
-                    data-animation-direction="">
-                    {category}
-                </a>
-                <Link to={`/catalog/${_id}`}
-                    className="u-border-active-palette-2-base u-border-hover-palette-1-base u-btn u-button-style u-none u-text-body-color u-text-hover-palette-2-light-1 u-btn-2"
-                    data-animation-name=""
-                    data-animation-duration="0"
-                    data-animation-delay="0"
-                    data-animation-direction="">
-                    {title}
-                </Link>
+        <div className="col-12 col-sm-6 col-lg-4">
+            <div className="single-best-receipe-area mb-30">
+                <img src={imageUrl} alt="" />
+                <div className="receipe-content">
+                    <Link to={`/catalog/${_id}`}>
+                        <h5>{title}</h5>
+                        <h6>{category}</h6>
+                    </Link>
+                </div>
             </div>
         </div>
     );
