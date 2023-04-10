bc064e1e078136b3c4c0a136c388463afed039f8
Simona Ivanova <116807754+simona-ivanova@users.noreply.github.com>
2023-04-10 12:03:58 +0300

    Edit About component

--- client/src/components/About/About.js
+++ client/src/components/About/About.js
@@ -1,12 +1,12 @@
 export const About = () => {
     return (
         <div>
-            <div className="breadcumb-area bg-img bg-overlay" style={{backgroundImage: `url("img/bg-img/2018_06_27_48298_1530062479._large.jpg")`}}>
+            <div className="breadcumb-area bg-img bg-overlay" style={{ backgroundImage: `url("img/bg-img/2018_06_27_48298_1530062479._large.jpg")` }}>
                 <div className="container h-100">
                     <div className="row h-100 align-items-center">
                         <div className="col-12">
                             <div className="breadcumb-text text-center">
-                                <h2>About us</h2>
+                                <h2>За мен</h2>
                             </div>
                         </div>
                     </div>
@@ -18,67 +18,31 @@ export const About = () => {
                     <div className="row">
                         <div className="col-12">
                             <div className="section-heading">
-                                <h3>Who we are and what we do?</h3>
+                                <h3>Здравейте!</h3>
+                                <h4>Много се радвам, че сте попаднали в моя малък вкусен свят!</h4>
                             </div>
                         </div>
                     </div>
 
                     <div className="row">
                         <div className="col-12">
-                            <h6 className="sub-heading pb-5">Donec quis metus ac arcu luctus accumsan. Nunc in justo tincidunt, sodales nunc id, finibus nibh. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Fusce nec ante vitae lacus aliquet vulputate. Donec scelerisque accumsan molestie. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae</h6>
-
-                            <p className="text-center">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum nec varius dui. Suspendisse potenti. Vestibulum ac pellentesque tortor. Aenean congue sed metus in iaculis. Cras a tortor enim. Phasellus posuere vestibulum ipsum, eget lobortis purus. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Proin malesuada et mauris ut lobortis. Sed eu iaculis sapien, eget luctus quam. Aenean hendrerit varius massa quis laoreet. Donec quis metus ac arcu luctus accumsan. Nunc in justo tincidunt, sodales nunc id, finibus nibh. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos.</p>
+                            <p className="text-center">Казвам се Таня и за мен създаването на ястия, които са едновременно вкусни и красиви, е моята истинска страст. Опитвам се да докажа, че тортите могат да бъдат красиви и без използването на фондан, както и вярвам, че всеки може да приготви вкусна храна у дома с правилната рецепта, верните продукти и напътствия от приятел. Аз ще бъда вашият верен приятел, ако искате да се потопите в дебрите на вкусната храна! Вярвам, че в моето любимо местенце има за всекиго по нещо и ще се радвам да намерите вашата муза тук при мен!</p>
                         </div>
                     </div>
 
                     <div className="row align-items-center mt-70">
 
-                        <div className="col-12 col-sm-6 col-lg-3">
-                            <div className="single-cool-fact">
-                                <img src="img/core-img/salad.png" alt="" />
-                                    <h3><span className="counter">1287</span></h3>
-                                    <h6>Amazing receipies</h6>
-                            </div>
-                        </div>
-
-
-                        <div className="col-12 col-sm-6 col-lg-3">
-                            <div className="single-cool-fact">
-                                <img src="img/core-img/hamburger.png" alt="" />
-                                    <h3><span className="counter">25</span></h3>
-                                    <h6>Burger receipies</h6>
-                            </div>
-                        </div>
-
-
-                        <div className="col-12 col-sm-6 col-lg-3">
-                            <div className="single-cool-fact">
-                                <img src="img/core-img/rib.png" alt="" />
-                                    <h3><span className="counter">471</span></h3>
-                                    <h6>Meat receipies</h6>
-                            </div>
-                        </div>
-
-
-                        <div className="col-12 col-sm-6 col-lg-3">
-                            <div className="single-cool-fact">
-                                <img src="img/core-img/pancake.png" alt="" />
-                                    <h3><span className="counter">326</span></h3>
-                                    <h6>Desert receipies</h6>
-                            </div>
-                        </div>
                     </div>
 
                     <div className="row">
                         <div className="col-12">
                             <img className="mb-70" src="img/bg-img/smiling-young-woman-chef-in-apron-standing-at-the-kitchen.jpg" alt="" />
-                                <p className="text-center">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum nec varius dui. Suspendisse potenti. Vestibulum ac pellentesque tortor. Aenean congue sed metus in iaculis. Cras a tortor enim. Phasellus posuere vestibulum ipsum, eget lobortis purus. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Proin malesuada et mauris ut lobortis. Sed eu iaculis sapien, eget luctus quam. Aenean hendrerit varius massa quis laoreet. Donec quis metus ac arcu luctus accumsan. Nunc in justo tincidunt, sodales nunc id, finibus nibh. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos.</p>
                         </div>
                     </div>
                 </div>
             </section>
 
-            
+
         </div>
     );
 };
