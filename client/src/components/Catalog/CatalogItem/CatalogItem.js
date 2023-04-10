c8b1f9a04867797b37627616c4024745b49048d2
Simona Ivanova <116807754+simona-ivanova@users.noreply.github.com>
2023-04-10 14:10:12 +0300

    Edit Catalog.css

--- client/src/components/Catalog/CatalogItem/CatalogItem.js
+++ client/src/components/Catalog/CatalogItem/CatalogItem.js
@@ -9,13 +9,16 @@ export const CatalogItem = ({
     return (
         <div className="col-12 col-sm-6 col-lg-4">
             <div className="single-best-receipe-area mb-30">
+                    <Link to={`/catalog/${_id}`}>
                 <img src={imageUrl1} alt="" loading="lazy"/>
                 <div className="receipe-content">
-                    <Link to={`/catalog/${_id}`}>
+                
+                        
                         <h5>{title}</h5>
                         <h6>{category}</h6>
-                    </Link>
+                    
                 </div>
+                </Link>
             </div>
         </div>
     );
