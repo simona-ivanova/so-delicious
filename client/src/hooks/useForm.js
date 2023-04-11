1c1e45951fb6dcdace178d73066231302eb971a0
Simona Ivanova <116807754+simona-ivanova@users.noreply.github.com>
2023-04-11 22:02:44 +0300

    Еxporting the validation to formValidationService.js file

--- client/src/hooks/useForm.js
+++ client/src/hooks/useForm.js
@@ -1,4 +1,5 @@
 import { useState } from "react";
+import { validateFormCreateRecipe, validateFormRegister } from "../services/formValidationService";
 
 export const useForm = (initialValues, onSubmitHandler) => {
     const [values, setValues] = useState(initialValues);
@@ -8,107 +9,6 @@ export const useForm = (initialValues, onSubmitHandler) => {
     const changeHandler = (e) => {
         setValues(state => ({ ...state, [e.target.name]: e.target.value }));
     };
-
-    const emailRegex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/g;
-    const passwordRegex = /^(?=\S{6,}$)[^\s<>&]*$/g;
-
-    function validateFormRegister(formData) {
-        const { firstName, lastName, email, password, repeatPassword } = formData;
-        const errors = {};
-
-        if (firstName.length < 1) {
-            errors.firstName = "Полето не може да бъде празно!";
-        } else if (firstName.trim().length < 2) {
-            errors.firstName = "Името трябва да е поне 2 символа!";
-        }
-
-        if (lastName.trim().length < 1) {
-            errors.lastName = "Полето не може да бъде празно!";
-        } else if (lastName.trim().length < 2) {
-            errors.lastName = "Фамилията трябва да бъде поне 2 символа!";
-        }
-
-        if (email.trim().length < 1) {
-            errors.email = "Полето не може да бъде празно!";
-        } else if (!emailRegex.test(email)) {
-            errors.email = "Невалиден имейл адрес!";
-        }
-
-        if (password.trim().length < 1) {
-            errors.password = "Полето не може да бъде празно!";
-        } else if (password.length < 6) {
-            errors.password = "Паролата трябва да бъде поне 6 символа! Празните пространства са забранени!";
-        }
-        else if (!passwordRegex.test(password)) {
-            errors.password = "Използвани са забранени символи!";
-        }
-
-        if (repeatPassword.trim() !== password.trim()) {
-            errors.repeatPassword = "Паролите не съвпадат!";
-        }
-
-        return errors;
-    }
-
-    function validateFormCreateRecipe(formData) {
-        const { title, category, imageUrl1, imageUrl2, prepTime, cookingTime, serving, ingredients, steps } = formData;
-        const errors = {};
-
-        if (!title) {
-            errors.title = "Полето не може да бъде празно!";
-        } else if (title.trim().length < 2) {
-            errors.title = "Името трябва да е поне 2 символа!";
-        }
-
-        if (!category) {
-            errors.category = "Избери категория!";
-        } else if (category === 'Избери категория') {
-             errors.category = "Невалидна категория!";
-        }
-
-        if (!serving) {
-            errors.serving = "Полето не може да бъде празно!";
-        } else if (serving < 1) {
-             errors.serving = "Минималното количество е 1!";
-        }
-
-        if (imageUrl1.trim().length < 1) {
-            errors.imageUrl1 = "Полето не може да бъде празно!";
-        } else if (!(imageUrl1.startsWith('http://') || imageUrl1.startsWith('https://'))) {
-            errors.imageUrl1 = "Невалиден адрес!";
-        }
-
-        if (imageUrl2.trim().length < 1) {
-            errors.imageUrl2 = "Полето не може да бъде празно!";
-        } else if (!(imageUrl2.startsWith('http://') || imageUrl2.startsWith('https://'))) {
-            errors.imageUrl2 = "Невалиден адрес!";
-        }
-
-        if (!prepTime) {
-            errors.prepTime = "Полето не може да бъде празно!";
-        } else if (prepTime < 5) {
-             errors.prepTime = "Минималното време е 5 минути!";
-        }
-
-        if (!cookingTime) {
-            errors.cookingTime = "Полето не може да бъде празно!";
-        } else if (cookingTime < 5) {
-             errors.cookingTime = "Минималното време е 5 минути!";
-        }
-
-        
-        if (!ingredients) {
-            errors.ingredients = "Полето не може да бъде празно!";
-        } 
-
-        if (!steps) {
-            errors.steps = "Полето не може да бъде празно!";
-        } 
-        
-        return errors;
-    }
-
-    
 
     const onSubmitRegister = (e) => {
         e.preventDefault();
---
+++ client/src/services/formValidationService.js
@@ -0,0 +1,98 @@
+const emailRegex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/g;
+const passwordRegex = /^(?=\S{6,}$)[^\s<>&]*$/g;
+
+export const validateFormRegister = (formData) => {
+    const { firstName, lastName, email, password, repeatPassword } = formData;
+    const errors = {};
+
+    if (firstName.length < 1) {
+        errors.firstName = "Полето не може да бъде празно!";
+    } else if (firstName.trim().length < 2) {
+        errors.firstName = "Името трябва да е поне 2 символа!";
+    }
+
+    if (lastName.trim().length < 1) {
+        errors.lastName = "Полето не може да бъде празно!";
+    } else if (lastName.trim().length < 2) {
+        errors.lastName = "Фамилията трябва да бъде поне 2 символа!";
+    }
+
+    if (email.trim().length < 1) {
+        errors.email = "Полето не може да бъде празно!";
+    } else if (!emailRegex.test(email)) {
+        errors.email = "Невалиден имейл адрес!";
+    }
+
+    if (password.trim().length < 1) {
+        errors.password = "Полето не може да бъде празно!";
+    } else if (password.length < 6) {
+        errors.password = "Паролата трябва да бъде поне 6 символа! Празните пространства са забранени!";
+    }
+    else if (!passwordRegex.test(password)) {
+        errors.password = "Използвани са забранени символи!";
+    }
+
+    if (repeatPassword.trim() !== password.trim()) {
+        errors.repeatPassword = "Паролите не съвпадат!";
+    }
+
+    return errors;
+}
+
+export const validateFormCreateRecipe = (formData) => {
+    const { title, category, imageUrl1, imageUrl2, prepTime, cookingTime, serving, ingredients, steps } = formData;
+    const errors = {};
+
+    if (!title) {
+        errors.title = "Полето не може да бъде празно!";
+    } else if (title.trim().length < 2) {
+        errors.title = "Името трябва да е поне 2 символа!";
+    }
+
+    if (!category) {
+        errors.category = "Избери категория!";
+    } else if (category === 'Избери категория') {
+         errors.category = "Невалидна категория!";
+    }
+
+    if (!serving) {
+        errors.serving = "Полето не може да бъде празно!";
+    } else if (serving < 1) {
+         errors.serving = "Минималното количество е 1!";
+    }
+
+    if (imageUrl1.trim().length < 1) {
+        errors.imageUrl1 = "Полето не може да бъде празно!";
+    } else if (!(imageUrl1.startsWith('http://') || imageUrl1.startsWith('https://'))) {
+        errors.imageUrl1 = "Невалиден адрес!";
+    }
+
+    if (imageUrl2.trim().length < 1) {
+        errors.imageUrl2 = "Полето не може да бъде празно!";
+    } else if (!(imageUrl2.startsWith('http://') || imageUrl2.startsWith('https://'))) {
+        errors.imageUrl2 = "Невалиден адрес!";
+    }
+
+    if (!prepTime) {
+        errors.prepTime = "Полето не може да бъде празно!";
+    } else if (prepTime < 5) {
+         errors.prepTime = "Минималното време е 5 минути!";
+    }
+
+    if (!cookingTime) {
+        errors.cookingTime = "Полето не може да бъде празно!";
+    } else if (cookingTime < 5) {
+         errors.cookingTime = "Минималното време е 5 минути!";
+    }
+
+    
+    if (!ingredients) {
+        errors.ingredients = "Полето не може да бъде празно!";
+    } 
+
+    if (!steps) {
+        errors.steps = "Полето не може да бъде празно!";
+    } 
+    
+    return errors;
+}
