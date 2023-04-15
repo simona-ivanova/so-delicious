60ea5b23bb7dae821be62db22e8448a159868222
Simona Ivanova <116807754+simona-ivanova@users.noreply.github.com>
2023-04-15 23:08:30 +0300

    Create CategoryItem.module.css

--- client/src/components/Catalog/Category/CategoryItem.js
+++ client/src/components/Catalog/Category/CategoryItem.js
@@ -1,3 +1,4 @@
+import styles from "./CategoryItem.module.css"
 import { Link } from "react-router-dom";
 
 export const CategoryItem = ({
@@ -8,15 +9,16 @@ export const CategoryItem = ({
 }) => {
     return (
         <div className="col-12 col-sm-6 col-lg-4">
+            <div className={styles["single-recipe"]}>
             <div className="single-best-receipe-area mb-30">
                 <Link to={`/catalog/${_id}`}>
-                    <img src={imageUrl1} alt="" loading="lazy" /> </Link>
+                    <img src={imageUrl1} alt="" className={styles["category-img"]} loading="lazy" /> </Link>
 
                 <div className="receipe-content">
                     <Link to={`/catalog/${_id}`}> <h5>{title}</h5> </Link>
                     {/* <Link to={`/catalog/category`}> <h6>{category}</h6> </Link> */}
                 </div>
-
+                </div>
             </div>
         </div>
     );
