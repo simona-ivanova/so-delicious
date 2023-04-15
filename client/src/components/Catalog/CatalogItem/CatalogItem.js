35dd4148cf2fe4391bea04c8d935f2622c63107f
Simona Ivanova <116807754+simona-ivanova@users.noreply.github.com>
2023-04-15 23:08:03 +0300

    Create CatalogItem.module.css

--- client/src/components/Catalog/CatalogItem/CatalogItem.js
+++ client/src/components/Catalog/CatalogItem/CatalogItem.js
@@ -1,3 +1,4 @@
+import styles from "./CatalogItem.module.css"
 import { Link } from "react-router-dom";
 
 export const CatalogItem = ({
@@ -8,15 +9,16 @@ export const CatalogItem = ({
 }) => {
     return (
         <div className="col-12 col-sm-6 col-lg-4">
-            <div className="single-best-receipe-area mb-30">
-                <Link to={`/catalog/${_id}`}>
-                    <img src={imageUrl1} alt="" loading="lazy" /> </Link>
+            <div className={styles["single-recipe"]}>
+                <div className="single-best-receipe-area mb-30">
+                    <Link to={`/catalog/${_id}`}>
+                        <img src={imageUrl1} alt="" className={styles["catalog-img"]} loading="lazy" /> </Link>
 
-                <div className="receipe-content">
-                    <Link to={`/catalog/${_id}`}> <h5>{title}</h5> </Link>
-                    {/* <Link to={`/catalog`}> <h6>{category}</h6> </Link> */}
+                    <div className="receipe-content">
+                        <Link to={`/catalog/${_id}`}> <h5>{title}</h5> </Link>
+                        {/* <Link to={`/catalog`}> <h6>{category}</h6> </Link> */}
+                    </div>
                 </div>
-
             </div>
         </div>
     );
