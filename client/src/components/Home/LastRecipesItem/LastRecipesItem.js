b7e5898b2f789ce6412260f05aea5aba609afccf
Simona Ivanova <116807754+simona-ivanova@users.noreply.github.com>
2023-04-15 23:10:15 +0300

    Create LastRecipesItem.module.css

--- client/src/components/Home/LastRecipesItem/LastRecipesItem.js
+++ client/src/components/Home/LastRecipesItem/LastRecipesItem.js
@@ -1,3 +1,4 @@
+import styles from "./LastRecipesItem.module.css";
 import { Link } from "react-router-dom";
 
 export const LastRecipesItem = ({
@@ -8,16 +9,18 @@ export const LastRecipesItem = ({
 }) => {
     return (
         <div className="col-12 col-sm-6 col-lg-4">
+            <div className={styles["last-recipes"]}>
             <div className="single-best-receipe-area mb-30">
                 <Link to={`/catalog/${_id}`}>
-                    <img src={imageUrl1} alt="" />
-                    <div className="receipe-content">
-
-                        <h5>{title}</h5>
-                        <h6>{category}</h6>
-
-                    </div>
+                    <img src={imageUrl1} alt="" className={styles["home-img"]} />
+                 
+                        <div className="receipe-content">
+                            <h5>{title}</h5>
+                            {/* <h6>{category}</h6> */}
+                        </div>
+        
                 </Link>
+            </div>
             </div>
         </div>
     );
